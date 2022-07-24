f9b1be3244998f918615607cdce0aeb7e584f327
DavidBohnMachado <44985128+DavidBohnMachado@users.noreply.github.com>
2022-07-24 17:17:28 +0100

    update cliente.py

--- new_GR_TP2/cliente.py
+++ new_GR_TP2/cliente.py
@@ -4,14 +4,29 @@ HOST = '127.0.0.1'     # Endereço IP do Servidor
 
 PORT = 5001     # Porta que o Servidor está
 
-# Criando a conexão
+def checkMessage(mensagem):
+    aux = 1
+    if(mensagem[0] == "help" and len(mensagem) == 1):
+        print("snmpget public localhost Id_oid ")
+        print("snmpset public localhost Id_oid (valuer)")
+        print("snmpgetnext public localhost Id_oid ")
+        print("snmpbulkget public localhost Min MAx  Id_oid")
+        print("snmpbulkget public localhost Id_oid \n ")
+        return aux
+    if(len(mensagem) > 3 and mensagem[1] == "public" and mensagem[2] == "localhost"):
+        aux = 0
+        return aux
+    return aux
 
+# Criando a conexão
 
 tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 destino = (HOST, PORT)
 
 tcp.connect(destino)
+
+
 
 print('\nDigite o comando')
 
@@ -20,18 +35,24 @@ print('Para sair use CTRL+X\n')
 
 # Recebendo a mensagem do usuário final pelo teclado
 
-mensagem = input()
+mensagem =""
 
+aux = 1
 # Enviando a mensagem para o Servidor TCP através da conexão
 
 while mensagem != '\x18':
+    while aux == 1:
+        mensagem = input()
+        print(mensagem)
+        arrMensagem = mensagem.split(" ")
+        print(arrMensagem)
+        aux = checkMessage(arrMensagem)
 
-   tcp.send(str(mensagem).encode())
-   msg=tcp.recv(1024)
-   mensagem= msg.decode('utf-8')
-   print(mensagem)
-
-   mensagem = input()
+    tcp.send(str(mensagem).encode())
+    msg = tcp.recv(1024)
+    mensagem = msg.decode('utf-8')
+    print(mensagem)
+    aux = 1
 
 # Fechando o Socket
 
